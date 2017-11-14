ec24246aa0f90d6cc7674fc7666b58b1330288d7
marszhanglin <780965203@qq.com>
2017-11-14 09:35:38 +0800

    maven
    maven

---
+++ maven/maven_no_model/src/main/resources/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Lenovo
+  Date: 2016/2/25
+  Time: 0:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="myController" method="post">
+    <input name="name">
+    return:${name}
+    <input value="提交" type="submit">
+</form>
+</body>
+</html>
--- zookeeper/zookeeperApiDemo/src/com/mars/zookeeper/zookeeperUtil.java
+++ zookeeper/zookeeperApiDemo/src/com/mars/zookeeper/zookeeperUtil.java
@@ -1,9 +1,33 @@
 package com.mars.zookeeper;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.zookeeper.KeeperException;
+
 public class zookeeperUtil {
 
-	public static void main(String[] args) {
-		
-	}
+	public void list(String groupNmae) throws KeeperException, InterruptedException{
+        String path ="/"+groupNmae;
+        try {
+            List<String> children = zk.getChildren(path, false);
+            if(children.isEmpty()){
+                System.out.printf("No memebers in group %s\n",groupNmae);
+                System.exit(1);
+            }
+            for(String child:children){
+                System.out.println(child);
+            }
+        } catch (KeeperException.NoNodeException e) {
+            System.out.printf("Group %s does not exist \n", groupNmae);
+            System.exit(1);
+        } 
+    }
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        ListGroup listGroup = new ListGroup();
+        listGroup.connect(args[0]);
+        listGroup.list(args[1]);
+        listGroup.close();
+    }
 
 }
